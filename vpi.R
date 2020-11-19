9572e0f76ca2953f41870d9146499f94f62993f6
Matthew Routley <matt@routleynet.org>
2020-11-19 12:09:01 -0500

    Save maps to disk

--- vpi.R
+++ vpi.R
@@ -42,23 +42,28 @@ vpi_geo_to <- vpi_geo[toronto_wards,]
 
 # Plot --------------------------------------------------------------------
 
-ed_zoom_map <- function(low = 1, high = 70000) {
+ed_zoom_map <- function(low = 1, high = 70000, filename = "ca_vpi") {
   p <- ggplot2::ggplot(data = dplyr::filter(vpi_geo, dplyr::between(electoral_district, low, high))) +
     ggplot2::geom_sf(ggplot2::aes(fill = VPI)) +
     ggplot2::scale_fill_fermenter("VPI", type = "div", palette = "BrBG") +
     ggplot2::theme(panel.background = ggplot2::element_blank(),
                    axis.text = ggplot2::element_blank(),
                    axis.ticks = ggplot2::element_blank())
+  ggplot2::ggsave(paste0(filename, ".png"))
   p
 }
 
-ca_vpi <- ed_zoom_map()
-south_ca_vpi <- ed_zoom_map(high = 60000)
-on_vpi <- ed_zoom_map(low = 35000, high = 36000)
-ab_vpi <- ed_zoom_map(low = 48000, high = 49000)
-bc_vpi <- ed_zoom_map(low = 59000, high = 60000)
+ed_zoom_map()
+ed_zoom_map(high = 60000, filename = "south_ca_vpi")
+ed_zoom_map(low = 35000, high = 36000, filename = "on_vpi")
+ed_zoom_map(low = 37000, high = 49000, filename = "midwest_vpi")
+ed_zoom_map(low = 59000, high = 60000, filename = "bc_vpi")
+ed_zoom_map(low = 24000, high = 25000, filename = "qc_vpi")
+ed_zoom_map(high = 23000, filename = "at_vpi")
 
-bbox_zoom_map <- function(xlim = c(-141.0181, -52.5823), ylim = c(41.67695, 89.99943)) { # TODO: Switch these to defaults, unless specified
+bbox_zoom_map <- function(xlim = c(-141.0181, -52.5823), 
+                          ylim = c(41.67695, 89.99943),
+                          filename = "ca_vpi") { # TODO: Switch these to defaults, unless specified
   p <- ggplot2::ggplot(data = vpi_geo) +
     ggplot2::geom_sf(ggplot2::aes(fill = VPI)) +
     coord_sf(xlim = xlim, ylim = ylim) +
@@ -66,12 +71,16 @@ bbox_zoom_map <- function(xlim = c(-141.0181, -52.5823), ylim = c(41.67695, 89.9
     ggplot2::theme(panel.background = ggplot2::element_blank(),
                    axis.text = ggplot2::element_blank(),
                    axis.ticks = ggplot2::element_blank())
+  ggplot2::ggsave(paste0(filename, "_zoom.png"))
   p
 }
 
-bbox_zoom_map()
-bbox_zoom_map(xlim = c(-81, -75), ylim = c(42, 46))
-
+bbox_zoom_map(xlim = c(-82, -76), ylim = c(42, 46), filename = "ggh_vpi") # GGH
+bbox_zoom_map(xlim = c(-119, -111), ylim = c(48, 55), filename = "ab_vpi") #AB
+bbox_zoom_map(xlim = c(-129, -121), ylim = c(48, 51.5), filename = "bc_vpi") #BC
+bbox_zoom_map(xlim = c(-76, -70), ylim = c(44.5, 47), filename = "qc_vpi") #QC
+bbox_zoom_map(xlim = c(-75, -53), ylim = c(43, 53), filename = "at_vpi") #AT
+bbox_zoom_map(xlim = c(-74, -73), ylim = c(45, 46), filename = "mt_vpi") #MT
 
 # Toronto districts
 ggplot2::ggplot(data = vpi_geo_to) +
@@ -79,4 +88,5 @@ ggplot2::ggplot(data = vpi_geo_to) +
   ggplot2::scale_fill_fermenter("VPI", type = "div", palette = "BrBG") +
   ggplot2::theme(panel.background = ggplot2::element_blank(),
                  axis.text = ggplot2::element_blank(),
-                 axis.ticks = ggplot2::element_blank())
+                 axis.ticks = ggplot2::element_blank()) +
+  ggplot2::ggsave("to_vpi.png")
