984461690d2fb920a4c266efff6a098e3660cfcb
Matthew Routley <matt@routleynet.org>
2020-11-19 11:30:13 -0500

    Switch to fermenter for scales

--- vpi.R
+++ vpi.R
@@ -36,14 +36,16 @@ vpi_geo <- dplyr::left_join(federal_shapefile, vpi_raw, by = c("FEDNUM" = "ED"))
   dplyr::rename(electoral_district = FEDNUM) %>% 
   dplyr::mutate(cut_vpi = ggplot2::cut_interval(vpi_geo$VPI, 9))
 
+vpi_geo$VPI[vpi_geo$VPI>2] <- 1.5
+
 vpi_geo_to <- vpi_geo[toronto_wards,]
 
 # Plot --------------------------------------------------------------------
 
 ed_zoom_map <- function(low = 1, high = 70000) {
   p <- ggplot2::ggplot(data = dplyr::filter(vpi_geo, dplyr::between(electoral_district, low, high))) +
-    ggplot2::geom_sf(ggplot2::aes(fill = cut_vpi)) +
-    ggplot2::scale_fill_brewer("VPI", palette = "YlOrBr", labels=c("Low", rep("", 6), "High")) +
+    ggplot2::geom_sf(ggplot2::aes(fill = VPI)) +
+    ggplot2::scale_fill_fermenter("VPI", type = "div", palette = "BrBG") +
     ggplot2::theme(panel.background = ggplot2::element_blank(),
                    axis.text = ggplot2::element_blank(),
                    axis.ticks = ggplot2::element_blank())
@@ -58,9 +60,9 @@ bc_vpi <- ed_zoom_map(low = 59000, high = 60000)
 
 bbox_zoom_map <- function(xlim = c(-141.0181, -52.5823), ylim = c(41.67695, 89.99943)) { # TODO: Switch these to defaults, unless specified
   p <- ggplot2::ggplot(data = vpi_geo) +
-    ggplot2::geom_sf(ggplot2::aes(fill = cut_vpi)) +
+    ggplot2::geom_sf(ggplot2::aes(fill = VPI)) +
     coord_sf(xlim = xlim, ylim = ylim) +
-    ggplot2::scale_fill_brewer("VPI", palette = "YlOrBr", labels=c("Low", rep("", 6), "High")) +
+    ggplot2::scale_fill_fermenter("VPI", type = "div", palette = "BrBG") +
     ggplot2::theme(panel.background = ggplot2::element_blank(),
                    axis.text = ggplot2::element_blank(),
                    axis.ticks = ggplot2::element_blank())
@@ -68,13 +70,13 @@ bbox_zoom_map <- function(xlim = c(-141.0181, -52.5823), ylim = c(41.67695, 89.9
 }
 
 bbox_zoom_map()
-bbox_zoom_map(xlim = c(-83, -75), ylim = c(42, 46))
+bbox_zoom_map(xlim = c(-81, -75), ylim = c(42, 46))
 
 
-
+# Toronto districts
 ggplot2::ggplot(data = vpi_geo_to) +
-  ggplot2::geom_sf(ggplot2::aes(fill = ggplot2::cut_interval(VPI, 9))) +
-  ggplot2::scale_fill_brewer("VPI", palette = "YlOrBr", labels=c("Low", rep("", 6), "High")) +
+  ggplot2::geom_sf(ggplot2::aes(fill = VPI)) +
+  ggplot2::scale_fill_fermenter("VPI", type = "div", palette = "BrBG") +
   ggplot2::theme(panel.background = ggplot2::element_blank(),
                  axis.text = ggplot2::element_blank(),
                  axis.ticks = ggplot2::element_blank())
